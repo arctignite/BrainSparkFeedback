f67c7035260db757b09768c0c8b94aba08f6f998
IJsbrand <ijsbrand.me@gmail.com>
2018-04-30 15:19:57 +0200

    tabledef

---
+++ tabledef.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+ 
+engine = create_engine('sqlite:///brainSparkRequests.db', echo=True)
+Base = declarative_base()
+ 
+########################################################################
+class Request(Base):
+    """"""
+    __tablename__ = "request"
+
+    id = Column(Integer, primary_key=True)
+    requesterID = Column(String)
+    resolutionRoomID = Column(String)
+ 
+    #----------------------------------------------------------------------
+    def __init__(self, requesterID, resolutionRoomID):
+        """"""
+        self.requesterID = requesterID
+        self.resolutionRoomID = resolutionRoomID
+ 
+# create tables
+Base.metadata.create_all(engine)
